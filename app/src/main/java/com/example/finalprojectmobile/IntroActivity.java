570a8d3dc6fc0e0487e391901cd175eaf424c99a
Kijin <kazamairoha186@gmail.com>
2025-05-13 11:26:05 +0700

    UI for Homescreen and Introscreen

---
+++ app/src/main/java/com/example/finalprojectmobile/IntroActivity.java
@@ -0,0 +1,12 @@
+package com.example.finalprojectmobile;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class IntroActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+    }
+} 
